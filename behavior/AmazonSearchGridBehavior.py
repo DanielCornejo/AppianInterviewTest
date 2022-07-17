37852c496f9e8a0feae492c18d37fea8c1cbcb36
DanielCornejo <shinylionx@gmail.com>
2022-07-17 22:27:43 +0200

    Add new class to support "Search item grid" page behaviors

---
+++ behavior/AmazonSearchGridBehavior.py
@@ -0,0 +1,23 @@
+from page.AmazonSearchGridPage import AmazonSearchGridPage
+
+
+class AmazonSearchGridBehavior(AmazonSearchGridPage):
+
+    def __init__(self, browser):
+        super().__init__(browser)
+
+    def access_to_item_by_index(self, index):
+        index_str = str(index)
+        self._access_item_by_index(index_str)
+
+    def is_search_loaded(self):
+        return self._search_loaded()
+
+    def search_is_correct(self, item):
+        name = self._get_name_item_grid("1")
+        if item.lower() in name.lower():
+            return True
+        else:
+            return False
+
+
