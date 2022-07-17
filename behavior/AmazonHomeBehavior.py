79564e1eac9aff4eeaa09f32aa91be693a843cc6
DanielCornejo <shinylionx@gmail.com>
2022-07-17 22:28:11 +0200

    Refactor to use new driver strategy

--- behavior/AmazonHomeBehavior.py
+++ behavior/AmazonHomeBehavior.py
@@ -3,16 +3,14 @@ from page.AmazonHomePage import AmazonHomePage
 
 class AmazonHomeBehavior(AmazonHomePage):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, browser):
+        super().__init__(browser)
 
     def access_home(self):
         self._access()
 
-    def search_item(self, item):
-        self._set_search_item(item)
-        self._click_search()
-
     def check_home(self):
         return self._is_at_home()
 
+    def are_category_recommendations_loaded(self):
+        return self._category_recommendation_load()
