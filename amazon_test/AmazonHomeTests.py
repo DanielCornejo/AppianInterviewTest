9cd1cb3e40ff7b1a438439f1c6381f9985a5d3ac
DanielCornejo <shinylionx@gmail.com>
2022-07-17 22:28:34 +0200

    Refactor to use new driver strategy and separate test cases

--- amazon_test/AmazonHomeTests.py
+++ amazon_test/AmazonHomeTests.py
@@ -1,26 +1,30 @@
 import unittest
 
 from behavior.AmazonHomeBehavior import AmazonHomeBehavior
+from framework.Browser import Browser
 
 
 class AmazonHomeTests(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.amazon_home_behavior = AmazonHomeBehavior()
+    def setUp(self):
+        self.browser = Browser()
+        self.amazon_home_behavior = AmazonHomeBehavior(self.browser)
 
     def test_sanity_check(self):
+        # Access Amazon Home
         self.amazon_home_behavior.access_home()
-        self.assertEqual(True, self.amazon_home_behavior.check_home())
+        self.assertEqual(True, self.amazon_home_behavior.check_home(), "Home loaded")
 
-    def test_search_item(self):
+    def test_category_recommendation_load(self):
+        # Access Amazon Home
         self.amazon_home_behavior.access_home()
-        self.assertEqual(True, self.amazon_home_behavior.check_home())
-        self.amazon_home_behavior.search_item("xiaomi")
+        self.assertEqual(True, self.amazon_home_behavior.check_home(), "Home loaded")
+        self.assertEqual(True, self.amazon_home_behavior.are_category_recommendations_loaded(),
+                         "Category recommendation loaded")
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.amazon_home_behavior.close()
+    def tearDown(self):
+        self.browser.close()
+
 
 if __name__ == '__main__':
     unittest.main()
