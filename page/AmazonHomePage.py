fc65de3c8ddcc27821f9819bb26644dc9eec44a9
DanielCornejo <shinylionx@gmail.com>
2022-07-17 22:26:17 +0200

    Refactor to use new driver strategy

--- page/AmazonHomePage.py
+++ page/AmazonHomePage.py
@@ -1,31 +1,25 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
-from  selenium.common.exceptions import TimeoutException
-
-from framework.Browser import Browser
 
 
-class AmazonHomePage(Browser):
+class AmazonHomePage:
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, browser):
+        self.__browser = browser
 
     def _access(self):
-        self._open_url("amazon.com")
-        self._maximize_window()
+        self.__browser.open_url("amazon.com")
+        self.__browser.maximize_window()
 
     def _is_at_home(self):
-        wait = WebDriverWait(self._get_driver(), 10)
-        try:
-            wait.until(expected_conditions.presence_of_element_located((By.ID, "nav-logo")))
+        locator = (By.ID, "nav-logo")
+        if self.__browser.wait_for_element_presence(locator, 10) is not None:
             return True
-        except TimeoutException:
+        else:
             return False
 
-    def _set_search_item(self, item):
-        self._get_element(By.ID, "twotabsearchtextbox").send_keys(item)
-
-    def _click_search(self):
-        self._get_element(By.ID, "nav-search-submit-button").click()
-
+    def _category_recommendation_load(self):
+        locator = (By.XPATH, "//div[@id='gw-card-layout']/div[contains(@id,'desktop')]")
+        if self.__browser.wait_for_elements_presence(locator, 10) is not None:
+            return True
+        else:
+            return False
